1ef76fb4f8a24c99bc4c440bb669571eebe5064f
Michael <michaelfu1998@gmail.com>
2021-02-18 10:23:39 +1100

    Create increment.py

---
+++ increment.py
@@ -0,0 +1,36 @@
+# increment 4
+
+# index > value
+# index 0 > 0
+
+# 1 > 7
+# 2 > 3
+# 3 > 1
+
+# 4 > 1
+# 5 > 5
+# 6 > 4
+
+# 7 > 9
+# 8 > 2
+# 9 > 3   
+
+# increment 3
+
+# index > value
+# index 0 > 0
+
+# 1 > 7
+# 2 > 4
+# 3 > 1
+
+# 4 > 8
+# 5 > 5
+# 6 > 2
+
+# 7 > 9
+# 8 > 6
+# 9 > 3   
+
+# divide into two index
+# fist index is (deck_size - 1) / increment
